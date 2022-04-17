c1ba711a23ce9b6029e86b43546440d55da250c9
MarkVed17 <vedantlahane17@gmail.com>
2022-04-17 23:30:37 +0530

    added Footer

---
+++ src/frontend/components/Footer/Footer.js
@@ -0,0 +1,55 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import "./Footer.css";
+
+function Footer() {
+  return (
+    <>
+      <footer className="footer-container">
+        {/* Logo + Copyright */}
+        <div className="footer-brand">
+          <Link to="/">
+            <img
+              className="footer-logo-img"
+              src="/assets/Logo/dino-tv-logo.svg"
+              alt="dino-tv-logo"
+            />
+          </Link>
+        </div>
+
+        {/* Social Media Presence */}
+        <div className="footer-social-media">
+          <h1 className="footer-heading">Connect with us</h1>
+          <span className="footer-icons">
+            <i className="fa-brands fa-instagram footer-icon"></i>
+            <i className="fa-brands fa-twitter footer-icon"></i>
+            <i className="fa-brands fa-facebook-f footer-icon"></i>
+          </span>
+        </div>
+
+        {/* Newsletter */}
+        <div className="footer-subscribe">
+          <h1 className="footer-heading">Subscribe</h1>
+          <div className="std-search">
+            <input
+              type="text"
+              placeholder="Enter Email Id"
+              className="input standard subscribe"
+            />
+            <button className="btn btn-brand btn-accent subscribe-btn">
+              Subscribe
+            </button>
+          </div>
+        </div>
+
+        {/* Copyright  */}
+        <div className="footer-copyright">
+          <span>Copyright © 2022 </span>
+          <span>All rights reserved</span>
+        </div>
+      </footer>
+    </>
+  );
+}
+
+export { Footer };
