cc43c238e645522e28cec87b52b8fc00392fd177
MarkVed17 <vedantlahane17@gmail.com>
2022-05-11 10:20:48 +0530

    sidebar barrel exports

--- src/frontend/components/index.js
+++ src/frontend/components/index.js
@@ -1,2 +1,5 @@
 export { Navbar } from "./Navbar/Navbar";
 export { Footer } from "./Footer/Footer";
+export { ShrinkedSidebar } from "./ShrinkedSidebar/ShrinkedSidebar";
+export { ExpandedSidebar } from "./ExpandedSidebar/ExpandedSidebar";
+export { Sidebar } from "./Sidebar/Sidebar";
