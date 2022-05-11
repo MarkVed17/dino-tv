dc3bc0481e248b553d5d68c4ba567601ae8aa8f9
MarkVed17 <vedantlahane17@gmail.com>
2022-05-11 10:21:27 +0530

    refactor: placeholder screens

--- src/frontend/screens/PlaylistScreen/PlaylistScreen.js
+++ src/frontend/screens/PlaylistScreen/PlaylistScreen.js
@@ -1,14 +1,7 @@
 import React from "react";
 
 const PlaylistScreen = () => {
-  return (
-    <div
-      className="main-content"
-      style={{ color: "white", textAlign: "center", marginTop: "10rem" }}
-    >
-      This is Playlist Screen
-    </div>
-  );
+  return <div style={{ color: "white" }}>This is Playlist Screen</div>;
 };
 
 export { PlaylistScreen };
--- src/frontend/screens/SearchScreen/SearchScreen.js
+++ src/frontend/screens/SearchScreen/SearchScreen.js
@@ -1,14 +1,7 @@
 import React from "react";
 
 const SearchScreen = () => {
-  return (
-    <div
-      className="main-content"
-      style={{ color: "white", textAlign: "center", marginTop: "10rem" }}
-    >
-      This is Search Screen
-    </div>
-  );
+  return <div style={{ color: "white"}}>This is Search Screen</div>;
 };
 
 export { SearchScreen };
