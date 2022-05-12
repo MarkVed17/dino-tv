385ae9a74d38e6acd42c1ad1297886f29b75d0a4
MarkVed17 <vedantlahane17@gmail.com>
2022-05-12 22:40:23 +0530

    db: added videos

--- src/backend/db/videos.js
+++ src/backend/db/videos.js
@@ -4,18 +4,301 @@
  * */
 
 export const videos = [
+  // Jurassic Park
   {
-    _id: "Wo5dMEP_BbI",
-    title: "Awesome Video about Coding",
+    _id: "PJlmYh27MHg",
+    title: "Jurassic Park (1993) - Welcome to Jurassic Park Scene",
+    views: 30283951,
+    uploadedAt: new Date("May 27, 2011"),
+    categoryName: "Jurassic Park",
     description:
-      "At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga.",
-    creator: "Soham Shah",
+      "Hammond (Richard Attenborough) leads the visitors, including Dr. Grant (Sam Neill) and Dr. Sattler (Laura Dern), to see their first live dinosaur - the Brachiosaur.",
   },
   {
-    _id: "F_Riqjdh2oM",
-    title: "Neural Networks from Scratch - P.1 Intro and Neuron Code",
-    creator: "Sentdex",
+    _id: "v5Co3A3fLBo",
+    title: "Jurassic Park (1993) - Tyrannosaurus Rex Scene",
+    views: 12789867,
+    uploadedAt: new Date("May 27, 2011"),
+    categoryName: "Jurassic Park",
     description:
-      "At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga.",
+      "Dr. Grant (Sam Neill) helps the children escape from the T-Rex, but the lawyer, Mr. Gennaro (Martin Ferrero), is eaten while hiding in the outhouse.",
+  },
+  {
+    _id: "d921M-ACMM4",
+    title: "Jurassic Park (1993) - Nedry's Plan Goes Awry Scene",
+    views: 6179768,
+    uploadedAt: new Date("May 27, 2011"),
+    categoryName: "Jurassic Park",
+    description:
+      "While attempting to make his escape, Nedry (Wayne Knight) encounters the poison-spitting Dilophosaur with tragic results.",
+  },
+  {
+    _id: "YKRnEOUxZm0",
+    title: "Jurassic Park (1993) - Raptor Clever Girl Scene",
+    views: 3818247,
+    uploadedAt: new Date("Jun 16, 2011"),
+    categoryName: "Jurassic Park",
+    description:
+      "While hunting one raptor, Muldoon (Bob Peck) is ambushed by a second Raptor and gets taken down.",
+  },
+  {
+    _id: "dnRxQ3dcaQk",
+    title: "Jurassic Park (1993) - Raptors in the Kitchen Scene",
+    views: 9830633,
+    uploadedAt: new Date("May 27, 2011"),
+    categoryName: "Jurassic Park",
+    description:
+      "Lex (Ariana Richards) and Tim (Joseph Mazzello) outwit the raptors that are hunting them and safely escape from the kitchen.",
+  },
+  {
+    _id: "gTWo9oLJOWk",
+    title: "Jurassic Park (1993) - T-Rex vs. the Raptors Scene",
+    views: 10802413,
+    uploadedAt: new Date("May 27, 2011"),
+    categoryName: "Jurassic Park",
+    description:
+      "Trapped by the Velociraptors, Dr. Grant (Sam Neill) and the others are saved at the last moment by the sudden arrival of the T-Rex.",
+  },
+  // The Lost World
+  {
+    _id: "CYGtLUZg1xA",
+    title: "The Lost World: Jurassic Park (1997) - Mommy's Very Angry",
+    views: 23288039,
+    uploadedAt: new Date("May 28, 2011"),
+    categoryName: "Jurasic Park 3",
+    description:
+      "A pair of T-Rexes track down their wounded offspring, putting Ian (Jeff Goldblum) and his team in mortal danger.",
+  },
+  {
+    _id: "mZ2kxdQf3t0",
+    title: "The Lost World: Jurassic Park (1997) - Ripped Apart",
+    views: 5943643,
+    uploadedAt: new Date("May 28, 2011"),
+    categoryName: "The Lost World",
+    description:
+      "Eddie (Richard Schiff) is ripped apart by two hungry Tyrannosaurs.",
+  },
+  {
+    _id: "9t8iEpdabms",
+    title: "The Lost World: Jurassic Park (1997) - T-Rex in the Tent",
+    views: 3268984,
+    uploadedAt: new Date("May 28, 2011"),
+    categoryName: "The Lost World",
+    description:
+      "The T-Rexes continue to stalk Dr. Harding (Julianne Moore) and Ian (Jeff Goldblum).",
+  },
+  {
+    _id: "2h8rH8zxA64",
+    title: "The Lost World: Jurassic Park (1997) - Raptor vs. Gymnast",
+    views: 3241827,
+    uploadedAt: new Date("May 28, 2011"),
+    categoryName: "The Lost World",
+    description:
+      "Kelly (Vanessa Lee Chester) uses her gymnast talents to save Ian (Jeff Goldblum) and Dr. Harding (Julianne Moore) from a deadly velociraptor ambush.",
+  },
+  {
+    _id: "w7tqVEdyteg",
+    title: "The Lost World: Jurassic Park (1997) - Backyard Dino",
+    views: 12631711,
+    uploadedAt: new Date("May 28, 2011"),
+    categoryName: "The Lost World",
+    description:
+      "The T-Rex takes a night stroll through a quiet San Diego neighborhood and terrorizes the inhabitants.",
+  },
+  {
+    _id: "cVIS31ghLNQ",
+    title: "The Lost World: Jurassic Park (1997) - Downtown Rampage",
+    views: 4968219,
+    uploadedAt: new Date("May 28, 2011"),
+    categoryName: "Jurassic Park",
+    description:
+      "A very pissed off T-Rex wreaks havoc through the streets of San Diego.",
+  },
+  // Jurassic Park 3
+  {
+    _id: "n7cRx_7umjE",
+    title: "Jurassic Park 3 (2001) - Crash Landing on Isla Sorna",
+    views: 7419048,
+    uploadedAt: new Date("May 28, 2011"),
+    categoryName: "The Lost World",
+    description:
+      "A pair of T-Rexes track down their wounded offspring, putting Ian (Jeff Goldblum) and his team in mortal danger.",
+  },
+  {
+    _id: "M7tNqjsclhs",
+    title: "Jurassic Park 3 (2001) - Spinosaurus vs. T-Rex",
+    views: 35588014,
+    uploadedAt: new Date("May 28, 2011"),
+    categoryName: "Jurassic Park 3",
+    description:
+      "Dr. Grant (Sam Neill) and the remaining survivors are trapped in a turf battle between a T-Rex and the ferocious Spinosaurus.",
+  },
+  {
+    _id: "jT8TUowrkLU",
+    title: "Jurassic Park 3 (2001) - Spinosaurus Attack!",
+    views: 10290504,
+    uploadedAt: new Date("May 28, 2011"),
+    categoryName: "Jurassic Park 3",
+    description:
+      "Stranded inside their plane, Dr. Grant (Sam Neil) and the survivors must endure the onslaught of the menacing Spinosaurus.",
+  },
+  {
+    _id: "trPOKL7Nguo",
+    title: "Jurassic Park 3 (2001) - Raptor Ambush Scene",
+    views: 7227112,
+    uploadedAt: new Date("May 28, 2011"),
+    categoryName: "Jurassic Park 3",
+    description:
+      "Dr. Grant (Sam Neill) and the survivors discover a deadly surprise at the abandoned genetics lab.",
+  },
+  {
+    _id: "2vOhL_Hw-gg",
+    title: "Jurassic Park 3 (2001) - What Are You Doing Here?",
+    views: 4023102,
+    uploadedAt: new Date("May 28, 2011"),
+    categoryName: "Jurassic Park 3",
+    description:
+      "Cornered by the Raptors, Dr. Grant (Sam Neill) is rescued by young Erik Kirby (Trevor Morgan).",
+  },
+  {
+    _id: "cVA4BO2v7zs",
+    title: "Jurassic Park 3 (2001) - A Broken Reunion",
+    views: 37846827,
+    uploadedAt: new Date("May 28, 2011"),
+    categoryName: "Jurassic Park 3",
+    description:
+      "Erik's (Trevor Morgan) reunion with his parents is interrupted by a hungry Spinosaurus.",
+  },
+  // Jurassic World
+  {
+    _id: "Q3znhTOUqi8",
+    title: "Jurassic World (2015) - Stand Down Scene",
+    views: 20381200,
+    uploadedAt: new Date("Nov 15, 2015"),
+    categoryName: "Jurassic World",
+    description:
+      "Owen (Chris Pratt) steps in to save a young handler after he falls into the raptors' cage.",
+  },
+  {
+    _id: "OjxcWhQYMKw",
+    title: "Jurassic World (2015) - It's In There With You Scene",
+    views: 38717485,
+    uploadedAt: new Date("Nov 15, 2015"),
+    categoryName: "Jurassic World",
+    description:
+      "The control team discovers that the Indominus Rex is still inside the cage with Owen (Chris Pratt) and the other workers.",
+  },
+  {
+    _id: "5ywvhn6Y4aE",
+    title: "Jurassic World (2015) - Indominus Attacks the Gyrosphere Scene",
+    views: 15052845,
+    uploadedAt: new Date("Nov 15, 2015"),
+    categoryName: "Jurassic World",
+    description:
+      "While exploring in the gyrosphere, Zack (Nick Robinson) and Gray (Ty Simpkins) are attacked by the Indominus Rex.",
+  },
+  {
+    _id: "sXHsY1eoIzA",
+    title: "Jurassic World (2015) T-Rex vs. Indominus Scene",
+    views: 13950165,
+    uploadedAt: new Date("Nov 15, 2015"),
+    categoryName: "Jurassic World",
+    description:
+      "Claire (Bryce Dallas Howard) releases the T-Rex to help fight Indominus.",
+  },
+  {
+    _id: "0FkeGnobtfo",
+    title: "Jurassic World (2015) - Raptors vs. Indominus Scene",
+    views: 4338585,
+    uploadedAt: new Date("Nov 15, 2015"),
+    categoryName: "Jurassic World",
+    description:
+      "Owen (Chris Pratt) regains leadership of the raptors, and together they attack the Indominus Rex.",
+  },
+  {
+    _id: "oPS8-oRvVh4",
+    title: "Jurassic World (2015) - Dinosaur Alliance Scene",
+    views: 9513043,
+    uploadedAt: new Date("Nov 15, 2015"),
+    categoryName: "Jurassic World",
+    description:
+      "Blue returns and joins the T-Rex in the fight against Indominus.",
+  },
+  // Fallen Kingdom
+  {
+    _id: "hAf3IBKWubo",
+    title: "Jurassic World: Fallen Kingdom (2018) - Mosasaurus Attack Scene",
+    views: 20286664,
+    uploadedAt: new Date("Sep 4, 2018"),
+    categoryName: "Fallen Kingdom",
+    description:
+      "A team sent to get a Indominus Rex rib runs into more than they bargained for.",
+  },
+  {
+    _id: "jwnPI-d36vU",
+    title: "Jurassic World: Fallen Kingdom (2018) - Reunited with Blue Scene",
+    views: 7906802,
+    uploadedAt: new Date("Sep 4, 2018"),
+    categoryName: "Fallen Kingdom",
+    description:
+      "Reunited with Blue: Owen (Chris Pratt) tracks down Blue, but so do Wheatley's (Ted Levine) men.",
+  },
+  {
+    _id: "BfRUV4g8sYw",
+    title: "Jurassic World: Fallen Kingdom (2018) - Baryonyx Attack Scene",
+    views: 9137553,
+    uploadedAt: new Date("Sep 4, 2018"),
+    categoryName: "Fallen Kingdom",
+    description:
+      "Franklin (Justice Smith) inadvertently allows a Baryonyx into their facility, along with lava.",
+  },
+  {
+    _id: "s-vP7WgMkpA",
+    title: "Jurassic World: Fallen Kingdom (2018) - Saved by Rexy Scene",
+    views: 42512226,
+    uploadedAt: new Date("Sep 4, 2018"),
+    categoryName: "Fallen Kingdom",
+    description:
+      "As a volcano erupts on the island, Claire (Bryce Dallas Howard) and Franklin (Justice Smith) are attacked by a carnotaurus. Thankfully, Rexy intervenes.",
+  },
+  {
+    _id: "XUSzvNtSsFE",
+    title:
+      "Jurassic World: Fallen Kingdom (2018) - T-Rex Blood Transfusion Scene",
+    views: 26993495,
+    uploadedAt: new Date("Sep 4, 2018"),
+    categoryName: "Fallen Kingdom",
+    description:
+      "Owen (Chris Pratt) and Claire (Bryce Dallas Howard) try to get T-Rex blood for a transfusion.",
+  },
+  {
+    _id: "xgX9WrVFO0Q",
+    title:
+      "Jurassic World: Fallen Kingdom (2018) - The Death of Jurassic Park Scene",
+    views: 7992480,
+    uploadedAt: new Date("Sep 4, 2018"),
+    categoryName: "Fallen Kingdom",
+    description:
+      "Claire (Bryce Dallas Howard), Owen (Chris Pratt), and Franklin (Justice Smith) outrace the erupting volcano. They view the tragic sight of a Brachiosaurus getting caught in the destruction of the island.",
+  },
+  {
+    _id: "CkAf_qzHNwo",
+    title:
+      "Jurassic World: Fallen Kingdom (2018) - The Jaws of the Indoraptor Scene",
+    views: 21962612,
+    uploadedAt: new Date("Sep 4, 2018"),
+    categoryName: "Fallen Kingdom",
+    description:
+      "Wheatley (Ted Levine) tries to get teeth from the wrong dinosaur.",
+  },
+  {
+    _id: "NFj82X1Fdh4",
+    title:
+      "Jurassic World: Fallen Kingdom (2018) - Welcome to Jurassic World Scene",
+    views: 28225826,
+    uploadedAt: new Date("Sep 4, 2018"),
+    categoryName: "Fallen Kingdom",
+    description:
+      "Dr. Ian Maclcolm (Jeff Goldblum) talks to the U.S. Senate about the new age brought upon by bringing Dinosaurs back into the world.",
   },
 ];
