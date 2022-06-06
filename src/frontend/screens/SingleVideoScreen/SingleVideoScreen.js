4606f341a53e134014ef49aa32785fa55ca9bd75
MarkVed17 <vedantlahane17@gmail.com>
2022-06-06 18:36:23 +0530

    feat: created a single video placeholder screen

---
+++ src/frontend/screens/SingleVideoScreen/SingleVideoScreen.js
@@ -0,0 +1,16 @@
+import React from "react";
+import {  } from "../../components";
+import "./SingleVideoScreen.css";
+
+const SingleVideoScreen = () => {
+
+  return (
+    <div className="main-content">
+      <div className="single-video-screen-content">
+        This is a single video
+      </div>
+    </div>
+  );
+};
+
+export { SingleVideoScreen };
