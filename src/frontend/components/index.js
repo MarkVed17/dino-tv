3bcd5d857a3d15c966767e5cfe20c09cf6985ce2
MarkVed17 <vedantlahane17@gmail.com>
2022-06-07 18:29:41 +0530

    barrel exported Single Video Screen components

--- src/frontend/components/index.js
+++ src/frontend/components/index.js
@@ -6,3 +6,6 @@ export { Sidebar } from "./Sidebar/Sidebar";
 export { PrivateRoute } from "./PrivateRoute/PrivateRoute";
 export { ExploreScreenCard } from "./ExploreScreenCard/ExploreScreenCard";
 export { FilterChip } from "./FilterChip/FilterChip";
+export { VideoPlayer } from "./VideoPlayer/VideoPlayer";
+export { SingleVideoDetails } from "./SingleVideoDetails/SingleVideoDetails";
+export { RecommendedVideosList } from "./RecommendedVideosList/RecommendedVideosList";
