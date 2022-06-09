543f76cd98b7782cad6f2e610614c4cd2ec2a8df
MarkVed17 <vedantlahane17@gmail.com>
2022-06-09 18:58:54 +0530

    updated `Single Video Details`

--- src/frontend/components/SingleVideoDetails/SingleVideoDetails.js
+++ src/frontend/components/SingleVideoDetails/SingleVideoDetails.js
@@ -1,32 +1,73 @@
 import React from "react";
 import Moment from "react-moment";
 import "./SingleVideoDetails.css";
+import { useAuth, useLikedVideos, useWatchLaterVideos } from "../../contexts";
+import { useNavigate } from "react-router-dom";
 
 const SingleVideoDetails = ({ video }) => {
+  const { _id, title, views, uploadedAt, description } = video;
+  const { auth } = useAuth();
+  const navigate = useNavigate();
+  const { likedVideos, likeVideoHandler, unlikeVideoHandler } =
+    useLikedVideos();
+  const {
+    watchLaterVideos,
+    addToWatchLaterHandler,
+    removeFromWatchLaterHandler,
+  } = useWatchLaterVideos();
+
   return (
     <div className="video-details">
-      <div className="video-title">{video.title}</div>
+      <div className="video-title">{title}</div>
       <div className="video-stats">
-        <span>{video.views} views</span>
+        <span>{views} views</span>
         <span className="material-icons video-card-secondary-divider">
           fiber_manual_record
         </span>
         <span>
-          <Moment fromNow>{video.uploadedAt}</Moment>
+          <Moment fromNow>{uploadedAt}</Moment>
         </span>
         <div className="video-actions">
-          <span
-            className="material-icons-outlined video-card-actions"
-            title="I like this"
-          >
-            thumb_up
-          </span>
-          <span
-            className="material-icons-outlined video-card-actions"
-            title="I will watch this later"
-          >
-            watch_later
-          </span>
+          {likedVideos.find((video) => video._id === _id) ? (
+            <span
+              className="material-icons video-card-actions"
+              title="I like this"
+              onClick={() => unlikeVideoHandler(_id)}
+            >
+              thumb_up
+            </span>
+          ) : (
+            <span
+              className="material-icons-outlined video-card-actions liked-video"
+              title="I like this"
+              onClick={() =>
+                auth.status ? likeVideoHandler(video) : navigate("/signin")
+              }
+            >
+              thumb_up
+            </span>
+          )}
+          {watchLaterVideos.find((video) => video._id === _id) ? (
+            <span
+              className="material-icons video-card-actions"
+              title="I will watch this later"
+              onClick={() => removeFromWatchLaterHandler(_id)}
+            >
+              watch_later
+            </span>
+          ) : (
+            <span
+              className="material-icons-outlined video-card-actions"
+              title="I will watch this later"
+              onClick={() =>
+                auth.status
+                  ? addToWatchLaterHandler(video)
+                  : navigate("/signin")
+              }
+            >
+              watch_later
+            </span>
+          )}
           <span
             className="material-icons-outlined video-card-actions"
             title="Add to a playlist"
@@ -35,7 +76,7 @@ const SingleVideoDetails = ({ video }) => {
           </span>
         </div>
       </div>
-      <div className="video-description">{video.description}</div>
+      <div className="video-description">{description}</div>
     </div>
   );
 };
