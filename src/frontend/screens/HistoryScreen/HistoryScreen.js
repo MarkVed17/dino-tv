bc384bb05fae62022c7fa48ede74dffd49e8f00e
MarkVed17 <vedantlahane17@gmail.com>
2022-06-10 02:02:08 +0530

    updated `History Screen`

--- src/frontend/screens/HistoryScreen/HistoryScreen.js
+++ src/frontend/screens/HistoryScreen/HistoryScreen.js
@@ -1,7 +1,50 @@
 import React from "react";
+import { useHistory } from "../../contexts";
+import "./HistoryScreen.css";
+import { HistoryVideoCard } from "../../components";
 
 const HistoryScreen = () => {
-  return <div style={{ color: "white" }}>This is History Screen</div>;
+  const { history, clearHistoryHandler } = useHistory();
+
+  return (
+    <div className="main-content">
+      {history.length ? (
+        <div className="history-videos-content">
+          <div className="history-video-banner">
+            <img
+              src={`https://img.youtube.com/vi/${
+                history[history.length - 1]._id
+              }/maxresdefault.jpg`}
+              alt="history Video Banner"
+              className="history-video-banner-img"
+            />
+            <div className="history-videos-banner-label">
+              <span className="material-icons history-videos-banner-icon">
+                history
+              </span>
+              <div className="history-videos-banner-text">History Videos</div>
+            </div>
+            <button
+              className="btn btn-brand btn-accent clear-history-btn"
+              onClick={() => clearHistoryHandler()}
+            >
+              Clear All
+            </button>
+          </div>
+
+          <div className="history-videos-list">
+            {history
+              .map((video) => (
+                <HistoryVideoCard key={video._id} video={video} />
+              ))
+              .reverse()}
+          </div>
+        </div>
+      ) : (
+        <div className="placeholder">No History</div>
+      )}
+    </div>
+  );
 };
 
 export { HistoryScreen };
